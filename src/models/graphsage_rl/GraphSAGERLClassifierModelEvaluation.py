1810d369c5a50d8daaa4151ac996bdf1fdd07c96
andreeaiana <andreeaiana@gmail.com>
2019-11-17 17:24:27 +0100

    Created classifier model + evaluation for graphsage_rl, and data preprocessing class

---
+++ src/models/graphsage_rl/GraphSAGERLClassifierModelEvaluation.py
@@ -0,0 +1,249 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
+
+sys.path.insert(0, os.path.join(os.getcwd(), "..", "evaluations"))
+sys.path.insert(0, os.path.join(os.getcwd(), "..", "..", "data"))
+sys.path.insert(0, os.path.join(os.getcwd(), "..", "..", "utils"))
+
+from DataLoader import DataLoader
+from EvaluationContainer import EvaluationContainer
+from GraphSAGERLClassifierModel import GraphSAGERLClassifierModel
+
+
+class GraphSAGERLClassifierModelEvaluation():
+
+    def __init__(self, classifier_name, embedding_type, graph_type,
+                 model_checkpoint, train_prefix, model_name,
+                 sampler_name="FastML", nonlinear_sampler=False,
+                 uniform_ratio=0.6, model_size="small", learning_rate=0.00001,
+                 epochs=10, dropout=0.0, weight_decay=0.0, max_degree=100,
+                 samples_1=25, samples_2=10, dim_1=128, dim_2=128,
+                 random_context=True, neg_sample_size=20, batch_size=512,
+                 identity_dim=0, save_embeddings=False,
+                 base_log_dir='../../../data/processed/graphsage/',
+                 validate_iter=5000, validate_batch_size=512, gpu=0,
+                 print_every=50, max_total_steps=10**10,
+                 log_device_placement=False, recs=10):
+
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
+
+        classifier = self._choose_classifier(classifier_name)
+
+        self.d_test = DataLoader()
+        self.d_train = DataLoader()
+
+        self.model = GraphSAGERLClassifierModel(
+                classifier, embedding_type, graph_type, model_checkpoint,
+                train_prefix, model_name, sampler_name, nonlinear_sampler,
+                uniform_ratio, model_size, learning_rate, epochs, dropout,
+                weight_decay, max_degree, samples_1, samples_2, dim_1, dim_2,
+                random_context, neg_sample_size, batch_size, identity_dim,
+                save_embeddings, base_log_dir, validate_iter,
+                validate_batch_size, gpu, print_every, max_total_steps,
+                log_device_placement, recs)
+
+    def _choose_classifier(self, classifier_name):
+        if classifier_name == "KNN":
+            classifier = KNeighborsClassifier(n_neighbors=30, n_jobs=10)
+        elif classifier_name == "MLP":
+            classifier = MLPClassifier(random_state=0, verbose=True)
+        elif classifier_name == "MultinomialLogisticRegression":
+            classifier = LogisticRegression(penalty="l2",
+                                            random_state=0,
+                                            solver="saga",
+                                            multi_class="multinomial",
+                                            verbose=1,
+                                            n_jobs=10)
+        else:
+            raise ValueError("Classifier name not recognised.")
+        return classifier
+
+    def evaluate(self):
+        # Train model if needed:
+        if not self.model._has_persistent_model():
+            training_data = self.d_train.training_data_with_abstracts_citations().data
+            self.model.train(training_data)
+
+        # Load test data
+        query_test, truth = self.d_test.evaluation_data_with_abstracts_citations()
+
+        self.model._load_model_classifier()
+
+        # Retrieve predictions
+        recommendation = self.model.query_batch(query_test)
+
+        # Evaluate
+        print("Evaluating...")
+        evaluation = EvaluationContainer()
+        evaluation.evaluate(recommendation, truth)
+
+    def main():
+        parser = argparse.ArgumentParser(
+                description='Arguments for unsupervised GraphSAGE model.')
+        parser.add_argument("classifier_name",
+                            choices=["KNN", "MLP",
+                                     "MultinomialLogisticRegression"],
+                            help="The name of the classifier.")
+        parser.add_argument('embedding_type',
+                            choices=["AVG_L", "AVG_2L", "AVG_SUM_L4",
+                                     "AVG_SUM_ALL", "MAX_2L",
+                                     "CONC_AVG_MAX_2L", "CONC_AVG_MAX_SUM_L4",
+                                     "SUM_L", "SUM_2L"
+                                     ],
+                            help="Type of embedding.")
+        parser.add_argument('graph_type',
+                            choices=["citations", "authors"],
+                            help="The type of graph used.")
+        parser.add_argument('model_checkpoint',
+                            help='Name of the GraphSAGE model checkpoint.')
+        parser.add_argument('train_prefix',
+                            help='Name of the object file that stores the '
+                            + 'training data.')
+        parser.add_argument('model_name',
+                            choices=["graphsage_mean", "gcn", "graphsage_seq",
+                                     "graphsage_maxpool", "graphsage_meanpool"
+                                     ],
+                            help="Model names.")
+        parser.add_argument('--sampler_name',
+                            choices=["Uniform", "ML", "FastML"],
+                            default="FastML",
+                            help="Sampler used.")
+        parser.add_argument("--nonlinear_sampler",
+                            action="store_true",
+                            default=False,
+                            help="Where to use nonlinear sampler o.w. " +
+                            "linear sampler"
+                            )
+        parser.add_argument("--uniform_ratio",
+                            type=int,
+                            default=0.6,
+                            help="In case of FastML sampling, the " +
+                            "percentile of uniform sampling preceding the " +
+                            "regressor sampling")
+        parser.add_argument('--model_size',
+                            choices=["small", "big"],
+                            default="small",
+                            help="Can be big or small; model specific def'ns")
+        parser.add_argument('--learning_rate',
+                            type=int,
+                            default=0.00001,
+                            help='Initial learning rate.')
+        parser.add_argument('--epochs',
+                            type=int,
+                            default=10,
+                            help='Number of epochs to train.')
+        parser.add_argument('--dropout',
+                            type=int,
+                            default=0.0,
+                            help='Dropout rate (1 - keep probability).')
+        parser.add_argument('--weight_decay',
+                            type=int,
+                            default=0.0,
+                            help='Weight for l2 loss on embedding matrix.')
+        parser.add_argument('--max_degree',
+                            type=int,
+                            default=100,
+                            help='Maximum node degree.')
+        parser.add_argument('--samples_1',
+                            type=int,
+                            default=25,
+                            help='Number of samples in layer 1.')
+        parser.add_argument('--samples_2',
+                            type=int,
+                            default=10,
+                            help='Number of users samples in layer 2.')
+        parser.add_argument('--dim_1',
+                            type=int,
+                            default=128,
+                            help='Size of output dim ' +
+                            '(final is 2x this, if using concat)')
+        parser.add_argument('--dim_2',
+                            type=int,
+                            default=128,
+                            help='Size of output dim ' +
+                            '(final is 2x this, if using concat)')
+        parser.add_argument('--random_context',
+                            action="store_false",
+                            default=True,
+                            help='Whether to use random context or direct ' +
+                            'edges.')
+        parser.add_argument('--neg_sample_size',
+                            type=int,
+                            default=20,
+                            help='Number of negative samples.')
+        parser.add_argument('--batch_size',
+                            type=int,
+                            default=512,
+                            help='Minibatch size.')
+        parser.add_argument('--identity_dim',
+                            type=int,
+                            default=0,
+                            help='Set to positive value to use identity ' +
+                            'embedding features of that dimension.')
+        parser.add_argument('--save_embeddings',
+                            action="store_true",
+                            default=False,
+                            help='Whether to save embeddings for all nodes ' +
+                            'after training')
+        parser.add_argument('--base_log_dir',
+                            default='../../../data/processed/graphsage/',
+                            help='Base directory for logging and saving ' +
+                            'embeddings')
+        parser.add_argument('--validate_iter',
+                            type=int,
+                            default=5000,
+                            help='How often to run a validation minibatch.')
+        parser.add_argument('--validate_batch_size',
+                            type=int,
+                            default=256,
+                            help='How many nodes per validation sample.')
+        parser.add_argument('--gpu',
+                            type=int,
+                            default=0,
+                            help='Which gpu to use.')
+        parser.add_argument('--print_every',
+                            type=int,
+                            default=50,
+                            help='How often to print training info.')
+        parser.add_argument('--max_total_steps',
+                            type=int,
+                            default=10**10,
+                            help='Maximum total number of iterations.')
+        parser.add_argument('--log_device_placement',
+                            action="store_true",
+                            default=False,
+                            help='Whether to log device placement.')
+        parser.add_argument('--recs',
+                            type=int,
+                            default=10,
+                            help='Number of recommendations.')
+        args = parser.parse_args()
+
+        from GraphSAGERLClassifierModelEvaluation import GraphSAGERLClassifierModelEvaluation
+        print("Starting...")
+        model = GraphSAGEClassifierModelEvaluation(
+                args.classifier_name, args.embedding_type, args.graph_type,
+                args.model_checkpoint, args.train_prefix, args.model_name,
+                args.model_name, args.nonlinear_sampler, args.uniform_ratio,
+                args.model_size, args.learning_rate, args.epochs, args.dropout,
+                args.weight_decay, args.max_degree, args.samples_1,
+                args.samples_2, args.dim_1, args.dim_2, args.random_context,
+                args.neg_sample_size, args.batch_size, args.identity_dim,
+                args.save_embeddings, args.base_log_dir, args.validate_iter,
+                args.validate_batch_size, args.gpu, args.print_every,
+                args.max_total_steps, args.log_device_placement, args.recs)
+        model.evaluate()
+        print("Finished.")
+
+    if __name__ == "__main__":
+        main()
---
+++ src/models/graphsage_rl/preprocess_data.py
@@ -0,0 +1,305 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import pandas as pd
+import numpy as np
+import json
+from tqdm import tqdm
+from collections import Counter
+from itertools import combinations
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.readwrite import json_graph
+from sklearn.preprocessing import StandardScaler
+
+from utils import run_random_walks
+
+sys.path.insert(0, os.path.join(os.getcwd(), "..", "..", "utils"))
+sys.path.insert(0, os.path.join(os.getcwd(), "..", "..", "data"))
+from TimerCounter import Timer
+from DataLoader import DataLoader
+from SciBERTEmbeddingsParser import EmbeddingsParser
+
+
+class Processor():
+
+    def __init__(self, embedding_type, graph_type, gpu=None):
+        self.embedding_type = embedding_type
+        self.graph_type = graph_type
+        self.embeddings_parser = EmbeddingsParser(gpu)
+        self.timer = Timer()
+        self.path_persistent = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "..", "..", "..", "data", "interim", "graphsage",
+                self.embedding_type, self.graph_type)
+        if not os.path.isdir(self.path_persistent):
+            os.mkdir(self.path_persistent)
+
+    def training_data(self, num_walks=50):
+        self.prefix = "train_val"
+        self.timer.tic()
+        print("Creating training files.")
+
+        # Load training and validation data
+        d_train = DataLoader()
+        df_train = d_train.training_data_with_abstracts_citations().data
+
+        d_val = DataLoader()
+        df_validation = d_val.validation_data_with_abstracts_citations().data
+
+        # Create and save graph
+        self.G = nx.Graph()
+
+        # Add nodes and edges
+        print("Adding training nodes.")
+        self._add_nodes(df_train, test=False, val=False)
+        print("Adding training edges.")
+        if self.graph_type == "authors":
+            df_train = d_train.author_names().data
+        self._add_edges(df_train)
+        print("Adding validation nodes.")
+        self._add_nodes(df_validation, test=False, val=True)
+        print("Adding validation edges.")
+        if self.graph_type == "authors":
+            df_validation = d_val.author_names().data
+        self._add_edges(df_validation)
+        print("Removing nodes without features.")
+        for node in list(self.G.nodes()):
+            if "feature" not in self.G.nodes[node].keys():
+                self.G.remove_node(node)
+        print("Nodes in graph: {}, edges in graph: {}.\n".format(
+                self.G.number_of_nodes(), self.G.number_of_edges()))
+
+        print("Saving graph to disk.")
+        G_data = json_graph.node_link_data(self.G)
+        with open(os.path.join(self.path_persistent, self.prefix + "-G.json"),
+                  "w") as f:
+            f.write(json.dumps(G_data))
+
+        # Create and save id map
+        self._create_id_map()
+
+        # Create and save features
+        self._create_features()
+
+        # Perform and save random walks
+        nodes = [n for n in list(self.G.nodes()) if not self.G.node[n]["val"]
+                 and not self.G.node[n]["test"]]
+        subgraph = self.G.subgraph(nodes)
+        self._run_random_walks(subgraph, nodes, num_walks)
+
+        print("Finished creating training files.")
+        self.timer.toc()
+
+        # print some statistics
+        self._get_stats()
+
+        # Plot degree histogram
+        self._degree_histogram()
+
+    def test_data(self, df_test, G_train, authors_df=None, normalize=True):
+        # TO DO: Add case for authors
+        self.prefix = "test"
+        print("Preprocessing data...")
+        self.G = G_train
+        print("Training graph has {} nodes and {} edges.\n".format(
+                self.G.number_of_nodes(), self.G.number_of_edges()))
+
+        # Add nodes and edges
+        print("Adding test nodes.")
+        self._add_nodes(df_test, test=True, val=False)
+        print("Adding test edges.")
+        if self.graph_type == "authors":
+            if authors_df is not None:
+                df_test = pd.merge(df_test, authors_df, how="left",
+                                   on=["chapter", "chapter"])
+            else:
+                raise ValueError("Chapter authors are missing.")
+        self._add_edges(df_test)
+        print("Removing nodes without features.")
+        for node in list(self.G.nodes()):
+            if "feature" not in self.G.nodes[node].keys():
+                self.G.remove_node(node)
+        print("Nodes in graph: {}, edges in graph: {}.\n".format(
+                self.G.number_of_nodes(), self.G.number_of_edges()))
+
+        # Remove all nodes that do not have val/test annotations
+        broken_count = 0
+        for node in self.G.nodes():
+            if 'val' not in self.G.node[node] or 'test' not in self.G.node[
+                    node]:
+                self.G.remove_node(node)
+                broken_count += 1
+        print("Removed {} nodes that lacked proper annotations due to networkx versioning issues.".format(
+                broken_count))
+
+        # Make sure the graph has edge train_removed annotations
+        for edge in self.G.edges():
+            if (self.G.node[edge[0]]['val'] or self.G.node[edge[1]]['val'] or
+               self.G.node[edge[0]]['test'] or self.G.node[edge[1]]['test']):
+                self.G[edge[0]][edge[1]]['train_removed'] = True
+            else:
+                self.G[edge[0]][edge[1]]['train_removed'] = False
+
+        # Create and process id map
+        id_map = self._create_id_map()
+
+        if isinstance(list(self.G.nodes)[0], int):
+            conversion = lambda n: int(n)
+        else:
+            conversion = lambda n: n
+        id_map = {conversion(k): int(v) for k, v in id_map.items()}
+
+        # Create and process features
+        features = self._create_features()
+
+        if normalize:
+            train_ids = np.array([id_map[n] for n in self.G.nodes() if not
+                                  self.G.node[n]['val'] and not
+                                  self.G.node[n]['test']])
+            train_feats = features[train_ids]
+            scaler = StandardScaler()
+            scaler.fit(train_feats)
+            features = scaler.transform(features)
+        print("Finished preprocessing data.")
+
+        # print some statistics
+        self._get_stats()
+
+        # Plot degree histogram
+        self._degree_histogram()
+
+        return self.G, features, id_map
+
+    def _add_nodes(self, data, test=False, val=False):
+        with tqdm(desc="Adding nodes: ", total=len(data), unit="node") as pbar:
+            for idx in range(len(data)):
+                self.G.add_node(
+                        data.chapter.iloc[idx],
+                        test=test,
+                        feature=np.concatenate((
+                                self.embeddings_parser.embed_sequence(
+                                        data.chapter_title.iloc[idx],
+                                        self.embedding_type),
+                                self.embeddings_parser.embed_sequence(
+                                        data.chapter_abstract.iloc[idx],
+                                        self.embedding_type)),
+                                axis=0).tolist(),
+                        val=val)
+                pbar.update(1)
+        print("Nodes in graph: {}.\n".format(self.G.number_of_nodes()))
+
+    def _add_edges(self, data):
+        if self.graph_type == "citations":
+            self._add_edges_citations(data)
+        elif self.graph_type == "authors":
+            self._add_edges_authors(data)
+        else:
+            raise KeyError("Graph type unknown.")
+
+    def _add_edges_citations(self, data):
+        """Adds edges between papers that share a citation.
+        """
+        with tqdm(desc="Adding edges: ", total=len(data)) as pbar:
+            for idx in range(len(data)):
+                self.G.add_edges_from(
+                        [(data.chapter.iloc[idx],
+                          data.chapter_citations.iloc[idx][i])
+                         for i in range(
+                                len(data.chapter_citations.iloc[idx]))])
+                pbar.update(1)
+        print("Edges in graph: {}.\n".format(self.G.number_of_edges()))
+
+    def _add_edges_authors(self, data):
+        """Adds edges between papers sharing an author.
+        """
+        data_grouped = data.groupby("author_name")["chapter"].agg(
+                list).reset_index()
+        with tqdm(desc="Adding edges: ", total=len(data_grouped)) as pbar:
+            for idx in range(len(data_grouped)):
+                self.G.add_edges_from(combinations(
+                        data_grouped.iloc[idx].chapter, 2))
+                pbar.update(1)
+        print("Edges in graph: {}.\n".format(self.G.number_of_edges()))
+
+    def _create_id_map(self):
+        if self.prefix == "train_val":
+            print("Creating id map.")
+
+        nodes = list(self.G.nodes)
+        id_map = {nodes[i]: i for i in range(len(nodes))}
+
+        if self.prefix == "test":
+            return id_map
+        else:
+            print("Saving id map to disk.")
+            with open(os.path.join(
+                    self.path_persistent, self.prefix + "-id_map.json"),
+                    "w") as f:
+                f.write(json.dumps(id_map))
+
+    def _create_features(self):
+        if self.prefix == "train_val":
+            print("Creating features.")
+
+        features = np.array([self.G.nodes[node]["feature"] for node in
+                             list(self.G.nodes)])
+
+        if self.prefix == "test":
+            return features
+        else:
+            print("Saving features to disk.")
+            np.save(os.path.join(self.path_persistent, self.prefix +
+                                 "-feats.npy"),
+                    features)
+
+    def _run_random_walks(self, graph, nodes, num_walks):
+        print("Running random walks.")
+        walks = run_random_walks(graph, nodes, num_walks=num_walks)
+        print("Saving random walks to disk.")
+        with open(os.path.join(
+                self.path_persistent, self.prefix + "-walks.txt"), "w") as fp:
+            fp.write("\n".join([str(w[0]) + "\t" + str(w[1]) for w in walks]))
+
+    def _get_stats(self):
+        degree_sequence = sorted([d for n, d in self.G.degree()], reverse=True)
+        degree_count = Counter(degree_sequence)
+
+        with open(os.path.join(
+                self.path_persistent, self.prefix + "-stats.txt"), "w") as fp:
+            self._print("Number of nodes in the graph: {}\n".format(
+                    self.G.number_of_nodes()), fp)
+            self._print("Number of edges in the graph: {}\n".format(
+                    self.G.number_of_edges()), fp)
+            self._print("The graph is connected: {}\n".format(
+                    nx.is_connected(self.G)), fp)
+            self._print("Number of connected components: {}\n".format(
+                    nx.number_connected_components(self.G)), fp)
+            self._print("Number of self-loops: {}\n".format(
+                    nx.number_of_selfloops(self.G)), fp)
+            self._print("Maximum degree: {}\n".format(max(degree_count)), fp)
+            self._print("Minimum degree: {}\n".format(min(degree_count)), fp)
+            self._print("Average degree: {}\n".format(
+                    sum(degree_sequence)/len(self.G)), fp)
+
+    def _degree_histogram(self):
+        # Plot degree histogram
+        degree_sequence = sorted([d for n, d in self.G.degree()], reverse=True)
+        degree_count = Counter(degree_sequence)
+        deg, cnt = zip(*degree_count.items())
+
+        fig, ax = plt.subplots()
+        plt.bar(deg, cnt, width=0.80, color='b')
+        plt.title("Degree Histogram")
+        plt.ylabel("Count")
+        plt.xlabel("Degree")
+        ax.set_xticks([d + 0.4 for d in deg])
+        ax.set_xticklabels(deg)
+
+        plt.savefig(os.path.join(
+                self.path_persistent, self.prefix + "-degree_histogram.png"),
+            bbox_inches="tight")
+
+    def _print(self, text, f):
+        print(text)
+        f.write(text)
