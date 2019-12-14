03d4ef8e20809e27fbec609e2941bf841d7c8e2f
andreeaiana <andreeaiana@gmail.com>
2019-12-14 13:04:04 +0100

    GAT Model and Evaluation classes

---
+++ src/models/gat/GATModel.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import json
+import pickle
+import random
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from networkx.readwrite import json_graph
+from sklearn.preprocessing import LabelEncoder
+
+sys.path.insert(0, os.path.join(os.getcwd(), ".."))
+sys.path.insert(0, os.path.join(os.getcwd(), "..", "..", "data"))
+sys.path.insert(0, os.path.join(os.getcwd(), "..", "..", "utils"))
+from TimerCounter import Timer
+from AbstractClasses import AbstractModel
+from preprocess_data import Processor
+from model import GATModel as GAT
+from DataLoader import DataLoader
+
+
+class GATModel(AbstractModel):
+
+    def __init__(self, embedding_type, dataset, hid_units=[256, 256],
+                 n_heads=[4, 4, 1], learning_rate=0.005, weight_decay=0,
+                 epochs=100000, batch_size=1, patience=100, residual=False,
+                 nonlinearity=tf.nn.elu, sparse=False, ffd_drop=0, attn_drop=0,
+                 gpu=0, recs=10):
+
+        self.embedding_type = embedding_type
+        self.dataset = dataset
+        self.recs = recs
+
+        self.gat_model = GAT(embedding_type, dataset, hid_units, n_heads,
+                             learning_rate, weight_decay, epochs, batch_size,
+                             patience, residual, nonlinearity, sparse,
+                             ffd_drop, attn_drop, None)
+        self.preprocessor = Processor(self.embedding_type, self.dataset, gpu)
+        self.training_data = self._load_training_data()
+
+    def query_single(self, query):
+        """Queries the model and returns a list of recommendations.
+
+        Args:
+            query (list): The query as needed by the model, is in the form
+            [chapter_title, chapter_abstract, list(chapter_citations)].
+
+        Returns
+            list: ids of the conferences
+            double: confidence scores
+        """
+        # Generate an ID for the query
+        if len(query) < 3:
+            raise ValueError("The input does not contain enough data; " +
+                             "chapter title, chapter abstract, and chapter " +
+                             "citations are required.")
+        return self.query_batch([(query[0], query[1], query[2])])
+
+    def query_batch(self, batch):
+        """Queries the model and returns a lis of recommendations.
+
+        Args:
+            batch (list of ntuples): The list of queries as needed
+            by the model. The ntuples are in the form (chapter_id,
+            chapter_title, chapter_abstract, list(chapter_citations)).
+
+        Returns
+            list: ids of the conferences
+            double: confidence scores
+        """
+        if len(batch) == 3:
+            df_test = pd.DataFrame(batch,
+                                   columns=["chapter_title",
+                                            "chapter_abstract",
+                                            "chapter_citations"])
+        else:
+            df_test_extended = pd.DataFrame(batch,
+                                            columns=["chapter",
+                                                     "chapter_title",
+                                                     "chapter_abstract",
+                                                     "chapter_citations"])
+            df_test = df_test_extended[["chapter_title", "chapter_abstract",
+                                        "chapter_citations"]]
+        train_features, train_labels, train_val_features, train_val_labels, graph = self.training_data
+
+        # Reindex test dataframe such that indices follow those from the
+        # train data
+        df_test.index = range(train_val_features.shape[0],
+                              train_val_features.shape[0] + len(df_test))
+
+        # Preprocess the data
+        test_data = self.preprocessor.test_data(
+                df_test, train_features, train_labels, train_val_features,
+                train_val_labels, graph)
+
+        # Inference on test data
+        predictions = self.gat_model.test(test_data)
+        print("Predictions: {}.".format(len(predictions)))
+
+        # Compute predictions
+        sorted_predictions = (-predictions).argsort(axis=1)
+        conferences = list()
+        confidences = list()
+
+        for i in range(len(predictions)):
+            one_hot_preds = np.zeros((self.recs, len(predictions[0])),
+                                     dtype=int)
+            for j in range(self.recs):
+                one_hot_preds[j][sorted_predictions[i, j]] = 1
+            conferences.append(list(self.label_encoder.inverse_transform(
+                    one_hot_preds).flatten()))
+            confidences.append(list(
+                    predictions[i, sorted_predictions[:, :self.recs][i]]))
+
+        results = [conferences, confidences]
+        return results
+
+    def train(self):
+        pass
+
+    def _load_training_data(self):
+        print("Loading training data.")
+        path_persistent = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "..", "..", "..", "data", "interim", "gat",
+                self.embedding_type, self.dataset)
+        names = ['x', 'y', 'allx', 'ally', 'graph']
+        objects = []
+        for i in range(len(names)):
+            with open(path_persistent + "/ind.{}.{}".format(
+                    self.dataset, names[i]), 'rb') as f:
+                objects.append(pickle.load(f, encoding='latin1'))
+        x, y, allx, ally, graph = tuple(objects)
+        return x, y, allx, ally, graph
+        print("Loaded.")
---
+++ src/models/gat/GATModelEvaluation.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+sys.path.insert(0, os.path.join(os.getcwd(), "..", "evaluations"))
+sys.path.insert(0, os.path.join(os.getcwd(), "..", "..", "data"))
+sys.path.insert(0, os.path.join(os.getcwd(), "..", "..", "utils"))
+
+from DataLoader import DataLoader
+from EvaluationContainer import EvaluationContainer
+from GATModel import GATModel
+
+
+class GATModelEvaluation:
+
+    def __init__(self, embedding_type, dataset, hid_units=[256, 256],
+                 n_heads=[4, 4, 1], learning_rate=0.005, weight_decay=0,
+                 epochs=100000, batch_size=1, patience=100, residual=False,
+                 nonlinearity=tf.nn.elu, sparse=False, ffd_drop=0, attn_drop=0,
+                 gpu=0, recs=10):
+
+        os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
+
+        self.d = DataLoader()
+        self.model = GATModel(embedding_type, dataset, hid_units, n_heads,
+                              learning_rate, weight_decay, epochs, batch_size,
+                              patience, residual, nonlinearity, sparse,
+                              ffd_drop, attn_drop, gpu, recs)
+
+    def evaluate(self):
+        # Load test data
+        query_test, truth = self.d.evaluation_data_with_abstracts_citations()
+
+        # Retrieve predictions
+        recommendation = self.model.query_batch(query_test)
+
+        # Evaluate
+        print("Evaluating...")
+        evaluation = EvaluationContainer()
+        evaluation.evaluate(recommendation, truth)
+
+    def main():
+        parser = argparse.ArgumentParser(
+                description='Arguments for unsupervised GraphSAGE model.')
+        parser.add_argument('embedding_type',
+                            choices=["AVG_L", "AVG_2L", "AVG_SUM_L4",
+                                     "AVG_SUM_ALL", "MAX_2L",
+                                     "CONC_AVG_MAX_2L", "CONC_AVG_MAX_SUM_L4",
+                                     "SUM_L", "SUM_2L"
+                                     ],
+                            help="Type of embedding.")
+        parser.add_argument('dataset',
+                            help='Name of the object file that stores the '
+                            + 'training data.')
+        parser.add_argument("--hid_units",
+                            type=int,
+                            nargs="+",
+                            default=[256, 256],
+                            help="Number of hidden units per each attention "
+                            + "head in each layer.")
+        parser.add_argument('--n_heads',
+                            type=int,
+                            nargs="+",
+                            default=[4, 4, 1],
+                            help='Additional entry for the output layer.')
+        parser.add_argument('--learning_rate',
+                            type=float,
+                            default=0.005,
+                            help='Learning rate.')
+        parser.add_argument('--weight_decay',
+                            type=float,
+                            default=0,
+                            help='Weight decay.')
+        parser.add_argument('--epochs',
+                            type=int,
+                            default=100000,
+                            help='Number of epochs to train.')
+        parser.add_argument('--batch_size',
+                            type=int,
+                            default=1,
+                            help='Batch size.')
+        parser.add_argument('--patience',
+                            type=int,
+                            default=100)
+        parser.add_argument('--residual',
+                            action="store_true",
+                            default=False)
+        parser.add_argument('--nonlinearity',
+                            help="Type of activation used")
+        parser.add_argument('--sparse',
+                            action='store_true',
+                            default=False,
+                            help="Whether to use the sparse model version")
+        parser.add_argument('--ffd_drop',
+                            type=float,
+                            default=0)
+        parser.add_argument('--attn_drop',
+                            type=float,
+                            default=0)
+        parser.add_argument('--gpu',
+                            type=int,
+                            default=0,
+                            help='Which gpu to use.')
+        parser.add_argument('--recs',
+                            type=int,
+                            default=10,
+                            help='Number of recommendations.')
+        args = parser.parse_args()
+
+        from GATModelEvaluation import GATModelEvaluation
+        print("Starting...")
+        model = GATModelEvaluation(
+                args.embedding_type, args.dataset, args.hid_units,
+                args.n_heads, args.learning_rate, args.weight_decay,
+                args.epochs, args.batch_size, args.patience, args.residual,
+                args.nonlinearity, args.sparse, args.ffd_drop, args.attn_drop,
+                args.gpu, args.recs)
+        model.evaluate()
+        print("Finished.")
+
+    if __name__ == "__main__":
+        main()
+
+
